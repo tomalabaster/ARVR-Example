4cd00c77401a0105c7058ce34bd1ceb9be9c4854
Tom Alabaster <tomalabasteruk@gmail.com>
2020-04-27 23:56:27 +0100

    Side by side ARSCNViews and corresponding scene set up

--- Room Scale VR Demo/ViewController.swift
+++ Room Scale VR Demo/ViewController.swift
@@ -11,23 +11,24 @@ import SceneKit
 import ARKit
 
 class ViewController: UIViewController, ARSCNViewDelegate {
-
-    @IBOutlet var sceneView: ARSCNView!
+    
+    @IBOutlet var leftEyeSceneView: ARSCNView!
+    @IBOutlet var rightEyeSceneView: ARSCNView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Set the view's delegate
-        sceneView.delegate = self
+        leftEyeSceneView.delegate = self
         
         // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
+        leftEyeSceneView.showsStatistics = true
         
         // Create a new scene
         let scene = SCNScene(named: "art.scnassets/ship.scn")!
         
         // Set the scene to the view
-        sceneView.scene = scene
+        leftEyeSceneView.scene = scene
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -37,39 +38,35 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         let configuration = ARWorldTrackingConfiguration()
 
         // Run the view's session
-        sceneView.session.run(configuration)
+        leftEyeSceneView.session.run(configuration)
+        
+        leftEyeSceneView.scene.background.contents = UIColor.black
+        
+        rightEyeSceneView.scene = leftEyeSceneView.scene
+        rightEyeSceneView.isPlaying = true
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         
         // Pause the view's session
-        sceneView.session.pause()
+        leftEyeSceneView.session.pause()
+        rightEyeSceneView.session.pause()
     }
-
-    // MARK: - ARSCNViewDelegate
     
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
-    }
-*/
-    
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
+    func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
+        let pov = leftEyeSceneView.pointOfView!.copy() as! SCNNode
+        let orientation = pov.orientation
+        let orientationQuaternion = GLKQuaternionMake(orientation.x, orientation.y, orientation.z, orientation.w)
+        let eyePos = GLKVector3Make(1.0, 0.0, 0.0)
+        let rotatedEyePos = GLKQuaternionRotateVector3(orientationQuaternion, eyePos)
+        let rotatedEyePosSCNV = SCNVector3Make(rotatedEyePos.x, rotatedEyePos.y, rotatedEyePos.z)
         
-    }
-    
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
+        let mag: Float = 0.086
+        pov.position.x += rotatedEyePosSCNV.x * mag
+        pov.position.y += rotatedEyePosSCNV.y * mag
+        pov.position.z += rotatedEyePosSCNV.z * mag
         
-    }
-    
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
+        rightEyeSceneView.pointOfView = pov
     }
 }
